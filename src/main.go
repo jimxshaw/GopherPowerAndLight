a5801dc906e6d9e4f29b1ba49466fed368c418f7
Jim Shaw <jimxshaw@gmail.com>
2017-04-01 15:57:29 -0500

    Refactored code into methods.

--- src/main.go
+++ src/main.go
@@ -7,6 +7,11 @@ func main() {
 	activePlants := []int{0, 1}
 	gridLoad := 75.
 
+	generateReport(plantCapacities, activePlants, gridLoad)
+
+}
+
+func generateReport(plantCapacities []float64, activePlants []int, gridLoad float64) {
 	fmt.Println("1) Generate Power Plant Report")
 	fmt.Println("2) Generate Power Grid Report")
 	fmt.Println("Please choose an option: ")
@@ -16,19 +21,26 @@ func main() {
 
 	switch option {
 	case "1":
-		for index, capa := range plantCapacities {
-			fmt.Printf("Plant %d capacity: %.0f\n", index, capa)
-		}
+		generatePlantCapacityReport(plantCapacities...)
 	case "2":
-		capacity := 0.
-		for _, plantId := range activePlants {
-			capacity += plantCapacities[plantId]
-		}
-		fmt.Printf("%-20s%.0f\n", "Capacity: ", capacity)
-		fmt.Printf("%-20s%.0f\n", "Load: ", gridLoad)
-		fmt.Printf("%-20s%.1f%%\n", "Utilization: ", gridLoad/capacity*100)
+		generatePowerGridReport(activePlants, plantCapacities, gridLoad)
 	default:
 		fmt.Println("Invalid choice. No action taken.")
 	}
+}
 
+func generatePlantCapacityReport(plantCapacities ...float64) {
+	for index, capa := range plantCapacities {
+		fmt.Printf("Plant %d capacity: %.0f\n", index, capa)
+	}
+}
+
+func generatePowerGridReport(activePlants []int, plantCapacities []float64, gridLoad float64) {
+	capacity := 0.
+	for _, plantId := range activePlants {
+		capacity += plantCapacities[plantId]
+	}
+	fmt.Printf("%-20s%.0f\n", "Capacity: ", capacity)
+	fmt.Printf("%-20s%.0f\n", "Load: ", gridLoad)
+	fmt.Printf("%-20s%.1f%%\n", "Utilization: ", gridLoad/capacity*100)
 }
