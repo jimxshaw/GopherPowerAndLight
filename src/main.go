5f86a9c211d20ebbe4a89d8473ccd8d5f35caaf8
Jim Shaw <jimxshaw@gmail.com>
2017-04-01 12:40:49 -0500

    Implemented basic console app.

--- src/main.go
+++ src/main.go
@@ -1,21 +1,34 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	var plantCapacities []float64
+	plantCapacities := []float64{30, 30, 60, 60, 100}
+	activePlants := []int{0, 1}
+	gridLoad := 75.
 
-	plantCapacities = []float64{30, 60, 100}
+	fmt.Println("1) Generate Power Plant Report")
+	fmt.Println("2) Generate Power Grid Report")
+	fmt.Println("Please choose an option: ")
 
-	var capacity = plantCapacities[0] + plantCapacities[1] + plantCapacities[2]
+	var option string
+	fmt.Scanln(&option)
 
-	var gridLoad = 75.
+	switch option {
+	case "1":
+		for index, capa := range plantCapacities {
+			fmt.Printf("Plant %d capacity: %.0f\n", index, capa)
+		}
+	case "2":
+		capacity := 0.
+		for _, plantId := range activePlants {
+			capacity += plantCapacities[plantId]
+		}
+		fmt.Printf("%-20s%.0f\n", "Capacity: ", capacity)
+		fmt.Printf("%-20s%.0f\n", "Load: ", gridLoad)
+		fmt.Printf("%-20s%.1f%%\n", "Utilization: ", gridLoad/capacity*100)
+	default:
+		fmt.Println("Invalid choice. No action taken.")
+	}
 
-	utilization := gridLoad / capacity
-
-	fmt.Printf("%-20s%.0f\n", "Capacity: ", capacity)
-	fmt.Printf("%-20s%.0f\n", "Load: ", gridLoad)
-	fmt.Printf("%-20s%.1f%%\n", "Utilization: ", utilization*100)
 }
