ffb643b0f1940105fcbad1c90d37e895be1690b5
Jim Shaw <jimxshaw@gmail.com>
2017-04-01 10:28:52 -0500

    Tested basic go program.

--- src/main.go
+++ src/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello Go!")
+	var plantCapacities []float64
+
+	plantCapacities = []float64{30, 60, 100}
+
+	var capacity = plantCapacities[0] + plantCapacities[1] + plantCapacities[2]
+
+	var gridLoad = 75.
+
+	utilization := gridLoad / capacity
+
+	fmt.Printf("%-20s%.0f\n", "Capacity: ", capacity)
+	fmt.Printf("%-20s%.0f\n", "Load: ", gridLoad)
+	fmt.Printf("%-20s%.1f%%\n", "Utilization: ", utilization*100)
 }
